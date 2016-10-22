8bedf37097d82590fad2ef1963909a4a313b391e
wangyw <wywwork@163.com>
2016-10-22 14:01:51 +0800

    modify bat Signed-off-by: wangyw <wywwork@163.com>

---
+++ chromiumstudy/Latex/build/win/tbrowser_article_build.bat
@@ -0,0 +1,13 @@
+@echo off
+
+cd ..\..\
+md build\output
+
+xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output '\def\withtbrowser{}\input{./articles/event_study.tex}'
+copy /Y  build\output\event_study.pdf out\
+
+
+
+cd build\win
+pause
+
