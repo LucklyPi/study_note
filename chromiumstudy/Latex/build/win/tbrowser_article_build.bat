50466940f59cdd2d4fe6c9f1c385dc594e7124f3
wangyw <wywwork@gmail.com>
2016-10-26 21:42:03 +0800

    key event pass

--- chromiumstudy/Latex/build/win/tbrowser_article_build.bat
+++ chromiumstudy/Latex/build/win/tbrowser_article_build.bat
@@ -3,7 +3,7 @@
 cd ..\..\
 md build\output
 
-xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output '\def\withtbrowser{}\input{./articles/event_study.tex}'
+xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output "\def\withtbrowser{}\input{./articles/event_study.tex}"
 copy /Y  build\output\event_study.pdf out\
 
 
