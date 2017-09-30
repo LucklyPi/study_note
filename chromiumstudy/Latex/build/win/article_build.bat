997c1ce6f3501dc17efa5c70035039f0b98eba0c
wangyw <wywwork@163.com>
2017-09-30 10:17:59 +0800

    使用参数的形式编译文档

--- chromiumstudy/Latex/build/win/article_build.bat
+++ chromiumstudy/Latex/build/win/article_build.bat
@@ -3,10 +3,30 @@
 cd ..\..\
 md build\output
 
-xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output "\input{./articles/event_study.tex}"
-copy /Y  build\output\event_study.pdf out\
+set /p build_article=you want build which article:
+ 
+if "%build_article%"=="event" (
+	echo build event article
+	set build_event_article=yes
+)
+if "%build_article%"=="process" (
+	echo build process article
+	set build_process_article=yes
+)
+if "%build_article%"=="all" (
+	echo build all article
+	set build_event_article=yes
+	set build_process_article=yes
+)
 
-
+if defined build_event_article (
+	xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output "\input{./articles/event_study.tex}"
+	copy /Y  build\output\event_study.pdf out\
+)
+if defined build_process_article (
+	xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output "\input{./articles/process_study.tex}"
+	copy /Y  build\output\process_study.pdf out\
+)
 
 cd build\win
 pause
--- chromiumstudy/Latex/build/win/tbrowser_article_build.bat
+++ chromiumstudy/Latex/build/win/tbrowser_article_build.bat
@@ -3,10 +3,21 @@
 cd ..\..\
 md build\output
 
-xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output "\def\withtbrowser{}\input{./articles/event_study.tex}"
-copy /Y  build\output\event_study.pdf out\
+set /p build_article=you want build which article:
 
+if "%build_article%"=="event" (
+	echo build event article
+	set build_event_article=yes
+)
+if "%build_article%"=="all" (
+	echo build all article
+	set build_event_article=yes
+)
 
+if defined build_event_article (
+    xelatex -synctex=1 -interaction=nonstopmode -output-directory=build/output '\def\withtbrowser{}\input{./articles/event_study.tex}'
+	copy /Y  build\output\event_study.pdf out\
+)
 
 cd build\win
 pause
